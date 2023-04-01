fedac16dfcc34c4e213df490977ef17d906c55ad
kahoragachau <joelkahoragachau6@gmail.com>
2023-04-01 10:50:11 +0300

    ✨feat: Add handleEdit function

--- src/components/TeachersCollection.js
+++ src/components/TeachersCollection.js
@@ -11,8 +11,12 @@ export default function TeachersCollection({ teachers, setTeachers }) {
         }))
       }
 
+      function handleEdit(teacher) {
+        
+      }
+
     const teacher = teachers?.map(teacher => {
-        return (<TeachersCard key={teacher.id} teacher={teacher} handleDelete={()=>handleDelete(teacher.id)}/>)
+        return (<TeachersCard key={teacher.id} teacher={teacher} handleDelete={()=>handleDelete(teacher.id)} handleEdit={() => handleEdit(teacher)} />)
     })
     return(
         <>
