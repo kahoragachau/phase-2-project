ccf0e8502e1439d2432993434f40b47e523c5dee
kahoragachau <joelkahoragachau6@gmail.com>
2023-04-03 14:23:15 +0300

    feat: Update correct path for redirect user to edit page

--- src/components/TeachersCollection.js
+++ src/components/TeachersCollection.js
@@ -12,14 +12,17 @@ export default function TeachersCollection({ teachers, setTeachers }) {
       }
 
       const navigate = useNavigate();
-      
-      function handleEdit(teacher) {
-        navigate('EditTeacher')
-        console.log(teacher)
+
+      function handleEdit(id) {
+         navigate(`/teacher/${id}/edit`)
       }
 
     const teacher = teachers?.map(teacher => {
-        return (<TeachersCard key={teacher.id} teacher={teacher} handleDelete={()=>handleDelete(teacher.id)} handleEdit={() => handleEdit(teacher)} />)
+        return (
+          <>
+          <TeachersCard key={teacher.id} teacher={teacher} handleDelete={()=>handleDelete(teacher.id)} handleEdit={() => {handleEdit(teacher.id)} } />
+          </>
+        )
     })
     return(
         <>
